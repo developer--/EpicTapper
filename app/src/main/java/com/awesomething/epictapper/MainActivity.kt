f9b52cb650252ee891d205af0db4e9b1686fb1dc
jemo mgebrishvili <jemo.mgebrishvili@gmail.com>
2016-09-23 12:58:46 +0400

    no message

--- app/src/main/java/com/awesomething/epictapper/MainActivity.kt
+++ app/src/main/java/com/awesomething/epictapper/MainActivity.kt
@@ -2,12 +2,18 @@ package com.awesomething.epictapper
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        attachListeners()
+    }
+
+    private fun attachListeners() {
+        androidButton.setOnClickListener { androidText.text = (androidText.text.toString().toLong() + 1).toString() }
     }
 
 
