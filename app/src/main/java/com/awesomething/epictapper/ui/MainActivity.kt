6f581d3f21f8ae392322f7e7302ac06efe67759c
jemo mgebrishvili <jemo.mgebrishvili@gmail.com>
2016-09-23 15:21:35 +0400

    scale

--- app/src/main/java/com/awesomething/epictapper/ui/MainActivity.kt
+++ app/src/main/java/com/awesomething/epictapper/ui/MainActivity.kt
@@ -1,13 +1,17 @@
-package com.awesomething.epictapper
+package com.awesomething.epictapper.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
+import com.awesomething.epictapper.R
 import com.awesomething.epictapper.model.data.MyData
 import com.awesomething.epictapper.presenter.MainPresenter
+import com.github.florent37.viewanimator.ViewAnimator
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener{
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit var presenter : MainPresenter
 
@@ -33,7 +37,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
     }
 
 
+
+    fun scaleView() {
+        ViewAnimator
+                .animate(androidButton)
+                .scale(1.05f,1f)
+                .duration(150)
+                .start()
+    }
+
     override fun onClick(v: View?) {
+        scaleView()
+
         val android = androidText.text.toString().toLong() + 1
         androidText.text = android.toString()
         presenter.save(android.toString())
